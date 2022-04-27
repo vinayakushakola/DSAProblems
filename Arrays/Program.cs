756e3fc2c3df5c877f7ec83493cb24b941cd9b1d
vinayak ushakola <vinayak.ushakola@gmail.com>
2022-04-28 00:20:04 +0530

    Added Subarray Problems

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Arrays
+﻿namespace Arrays
 {
     class Program
     {
+        /* Arrays
+           1. Prefix Sum (Range Sum Queries)
+           2. Carry Forward Technique
+           3. Sliding Window Technique (Length K)
+           4. Contribution Technique
+        */
         static void Main(string[] args)
         {
             #region Input 
-            List<int> arr = new List<int>{ 1,2,3,4,5 };
-            var res = PrefixArrayProblems.ProductArray(arr);
+            int[] A = { 1, 2, 3, 4, 5 };
+            SubarrayProblems.MaxSubarraySum(A);
             #endregion        
         }
     }
---
+++ Arrays/SubarrayProblems.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Arrays
+{
+    class SubarrayProblems
+    {
+        // Print all subarrays sum
+        public static void PrintSubarraySum(int[] A)
+        {
+            int size = A.Length;
+            int subarrayCnt = 0;
+            for (int i = 0; i<size; i++)
+            {
+                for (int j = i; j < size; j++)
+                {
+                    int sum = 0;
+                    for (int k = i; k<=j; k++)
+                    {
+                        sum += A[k];
+                    }
+                    subarrayCnt++;
+                    Console.WriteLine(subarrayCnt +". "+ sum);
+                }
+            }
+        } 
+
+        // Print Max Subarray Sum
+        public static void MaxSubarraySum(int[] A)
+        {
+            // Carry Forward Technique TC - O(N^2) | SC - O(1)
+            int size = A.Length;
+            int max_sum = int.MinValue;
+            for (int i = 0; i<size; i++)
+            {
+                int sum = 0;
+                for (int j=i; j<size; j++)
+                {
+                    sum += A[j];
+                    max_sum = Math.Max(max_sum, sum);
+                }
+            }
+            Console.WriteLine("Max Subarray Sum = " + max_sum);
+        }
+
+        // Print Max Subaaray Sum with Length K
+        public static void MaxSubarraySumWithK(int[] A, int k)
+        {
+            #region Brute Force TC - O(N^2) SC - O(1)
+            //int max_sum = int.MinValue;
+            //int size = A.Length;
+            //for(int i=0; i<=size-k; i++)
+            //{
+            //    int x = i + k - 1;
+            //    int sum = 0;
+            //    for (int j = i; j <= x; j++)
+            //    {
+            //        sum += A[j];
+            //    }
+            //    max_sum = Math.Max(max_sum, sum);
+            //}
+            //Console.WriteLine("Max Sum = " + max_sum);
+            #endregion
+            #region Sliding Window Technique TC - O(N) | SC - O(1)
+            int size = A.Length;
+            int max_sum = 0, sum = 0;
+            for (int  i = 0; i<k; i++)
+            {
+                sum += A[i];
+            }
+            max_sum = sum;
+            for (int i = 1; i <size; i++)
+            {
+                sum -= A[i - k];
+                sum += A[i];
+                max_sum = Math.Max(max_sum, sum);
+            }
+            #endregion
+        }
+
+        // Sum of all subarray sum - Google|Bloomberg|Amazon
+        public static void SumOfAllSubarrays(int[] A)
+        {
+            int sum = 0, size = A.Length;
+            for(int i = 0; i<size; i++)
+            {
+                int appears = (i + 1) * (size - i);
+                int contribution = A[i] * appears;
+                sum += contribution;
+            }
+            Console.WriteLine("Sum of all Subarrays = "+sum);
+        }
+    }
+}
